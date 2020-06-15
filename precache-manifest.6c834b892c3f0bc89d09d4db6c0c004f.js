6f79f221852f8902b8451b5f6a1de03aa2d1fa10
AbirNadav <pakakaw41@gmail.com>
2020-06-15 14:48:36 +0300

    Updates

---
+++ precache-manifest.6c834b892c3f0bc89d09d4db6c0c004f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3705154f83275c29177",
+    "url": "/MyProjects/static/css/main.b0d423e8.chunk.css"
+  },
+  {
+    "revision": "f3705154f83275c29177",
+    "url": "/MyProjects/static/js/main.f3705154.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/MyProjects/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "b027af441cf480aaa615",
+    "url": "/MyProjects/static/js/runtime~main.b027af44.js"
+  },
+  {
+    "revision": "a08c46b6e4a67ca57e01e40f12a703ef",
+    "url": "/MyProjects/static/media/bgi.a08c46b6.jpg"
+  },
+  {
+    "revision": "78c374dff1a4f760e3ca3659f14326cb",
+    "url": "/MyProjects/index.html"
+  }
+];
